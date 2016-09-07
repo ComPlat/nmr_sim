0612728006f996411cf2055adbf1e08f0e8ab3c6
jasonych99 <jasonych99@gmail.com>
2016-09-07 09:53:10 +0200

    include associations for the main app

--- lib/nmr_sim/engine.rb
+++ lib/nmr_sim/engine.rb
@@ -13,14 +13,20 @@ module NmrSim
       end
     end
 
-    # keep migration in engine
     initializer :append_migrations do |app|
+      # keep migration in engine
       unless app.root.to_s.match root.to_s
         config.paths["db/migrate"].expanded.each do |expanded_path|
           app.config.paths["db/migrate"] << expanded_path
         end
       end
-    end
 
+      # create associations for main app.
+      Molecule.class_eval do
+        has_one :nmr_simulation, class_name: NmrSimulation,
+                foreign_key: "molecule_id",
+                dependent: :destroy
+      end
+    end
   end
 end
